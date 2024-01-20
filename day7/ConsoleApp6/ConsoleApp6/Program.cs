449af197c70b4a1aa14e6f7fe6c1f97123488708
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-01-20 13:25:34 +0530

    [Arun] Add UC3

--- day7/ConsoleApp6/ConsoleApp6/Program.cs
+++ day7/ConsoleApp6/ConsoleApp6/Program.cs
@@ -8,14 +8,26 @@ namespace ConsoleApp6
 {
     internal class Program
     {
-        static int UC1()
-        {
-            int position = 0;
-            return position;
-        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine($"Player 1 position at start = {UC1()}");
+            int position = 0;
+            Random rand = new Random();
+            int number = rand.Next(1, 7);
+            int option = rand.Next(0, 3);
+
+            switch(option)
+            {
+                case 0:
+                    Console.WriteLine("Player stays in same location");
+                    break;
+                case 1:
+                    Console.WriteLine(number + position);
+                    break;
+                case 2:
+                    Console.WriteLine(position - number);
+                    break;
+            }
             Console.ReadLine();
         }
     }
