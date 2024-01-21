1c2333f94ba7e853e0232b952f4fec1394ca6e5a
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-01-21 20:26:54 +0530

    [Add] Adding UC4

--- day7/ConsoleApp6/ConsoleApp6/Program.cs
+++ day7/ConsoleApp6/ConsoleApp6/Program.cs
@@ -11,24 +11,39 @@ namespace ConsoleApp6
         
         static void Main(string[] args)
         {
-            int position = 0;
             Random rand = new Random();
-            int number = rand.Next(1, 7);
-            int option = rand.Next(0, 3);
 
-            switch(option)
+            int position = 0;
+            int winningposition = 100;
+
+            while (position < winningposition)
             {
-                case 0:
-                    Console.WriteLine("Player stays in same location");
-                    break;
-                case 1:
-                    Console.WriteLine(number + position);
-                    break;
-                case 2:
-                    Console.WriteLine(position - number);
-                    break;
+                int number = rand.Next(1, 7);
+                int option = rand.Next(0, 3);
+
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine($"No Play. Player stays at position {position}");
+                        break;
+                    case 1:
+                        position += number;
+                        Console.WriteLine($"Ladder! Player moves ahead to {position}");
+                        break;
+                    case 2:
+                        position -= number;
+
+                        if (position < 0)
+                        {
+                            position = 0;
+                        }
+                        Console.WriteLine($"Snake! Player moves back to {position}");
+                        break;
+
+
+                }
             }
             Console.ReadLine();
-        }
-    }
+        }
+    }
 }
